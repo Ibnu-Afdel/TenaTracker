700c7487b8cec0252711221510d1591bb1a429ea
Ibnu Afdel <ibnuafdel@gmail.com>
2025-01-29 23:05:53 +0300

    feat: Add challenges model and migration with the logic included

---
+++ app/Models/Challenge.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Challenge extends Model
+{
+    /** @use HasFactory<\Database\Factories\ChallengeFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'user_id', 'start_date', 'end_date', 'status', 'is_favorite'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    // public function tags(): BelongsToMany{
+    //     return $this->belongsToMany(Tag::class, 'challenge_tag');
+    // }
+
+    // public function journalEntries(): HasMany {
+    //     return $this->hasMany(JournalEntry::class);
+    // }
+
+    // public function remiders():HasMany {
+    //     return $this->hasMany(Reminder::class);
+    // }
+
+}
