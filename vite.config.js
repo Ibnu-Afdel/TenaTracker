6d88d3ab8884da279a70b62f6b865077071e5bdf
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-03 01:45:00 +0300

    fix CORS error for client

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,23 @@
 // });
 
 
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+//     server: {
+//         hmr: {
+//             host: 'localhost',
+//         },
+//     },
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -21,9 +38,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-    },
+    // Remove the server configuration for production
+    // server: {
+    //     hmr: {
+    //         host: 'localhost',
+    //     },
+    // },
 });
