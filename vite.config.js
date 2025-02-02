91396b3d2a97ca55c857500b7e30a30568b691b5
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-03 02:03:28 +0300

    fix vite for production

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    }
+});
+
+
+
 // import { defineConfig } from 'vite';
 // import laravel from 'laravel-vite-plugin';
 
@@ -28,20 +46,23 @@
 //     },
 // });
 
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    // Remove the server configuration for production
-    // server: {
-    //     hmr: {
-    //         host: 'localhost',
-    //     },
-    // },
-});
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+//     // Remove the server configuration for production
+//     // server: {
+//     //     hmr: {
+//     //         host: 'localhost',
+//     //     },
+//     // },
+// });
