5dcb15d40e66603d7e096a165002e884aa15f1c8
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-02 21:30:44 +0300

    Fix SQLite path for Railway deployment

--- vite.config.js
+++ vite.config.js
@@ -11,53 +11,6 @@
 // });
 
 
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-//     server: {
-//         hmr: {
-//             host: 'localhost',
-//         },
-//     },
-// });
-
-
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-//     build: {
-//         manifest: true,
-//         outDir: 'public/build',
-//     },
-//     server: {
-//         strictPort: true,
-//         hmr: {
-//             host: 'tenatracker-production.up.railway.app',
-//             protocol: 'wss', // WebSocket Secure
-//         },
-//         cors: {
-//             origin: '*', // Allow all origins
-//             credentials: true, // Allow credentials if needed
-//         }
-//     },
-// });
-
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -69,16 +22,8 @@ export default defineConfig({
         }),
     ],
     server: {
-        strictPort: true,
-        hmr: process.env.APP_ENV === 'local' ? { host: 'localhost' } : false, // Prevents mixed content in production
-    },
-    build: {
-        outDir: 'public/build', // Ensure the correct build folder
-        manifest: true,
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-            },
+        hmr: {
+            host: 'localhost',
         },
     },
 });
