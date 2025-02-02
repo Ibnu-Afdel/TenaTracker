064b636417379603483413d0cb6e2d82f60aec54
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-02 16:12:09 +0300

    rollback vite config

--- vite.config.js
+++ vite.config.js
@@ -30,54 +30,29 @@
 
 
 
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//         livewire(),
-//     ],
-//     build: {
-//         manifest: true,
-//         outDir: 'public/build',
-//     },
-//     server: {
-//         strictPort: true,
-//         hmr: {
-//             host: 'tenatracker-production.up.railway.app',
-//             protocol: 'wss', // WebSocket Secure
-//         },
-//         cors: {
-//             origin: '*', // Allow all origins
-//             credentials: true, // Allow credentials if needed
-//         }
-//     },
-// });
-
-
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  build: {
-    manifest: true,
-    outDir: 'public/build',
-  },
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      host: process.env.VITE_APP_URL,
-      protocol: 'wss'
-    }
-  }
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    },
+    server: {
+        strictPort: true,
+        hmr: {
+            host: 'tenatracker-production.up.railway.app',
+            protocol: 'wss', // WebSocket Secure
+        },
+        cors: {
+            origin: '*', // Allow all origins
+            credentials: true, // Allow credentials if needed
+        }
+    },
+});
