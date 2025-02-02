524d6dd0de40538d8b40e3e80f365978bfcc4a4e
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-02 15:07:00 +0300

    Fix Vite assets issue

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,23 @@
 // });
 
 
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+//     server: {
+//         hmr: {
+//             host: 'localhost',
+//         },
+//     },
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -21,9 +38,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    },
     server: {
         hmr: {
-            host: 'localhost',
+            host: 'tenatracker-production.up.railway.app',
         },
     },
 });
+
