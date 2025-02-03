aceeda92ead9df54f6fefd016d995c12e747b649
Ibnu Afdel <ibnuafdel@gmail.com>
2025-02-03 12:07:39 +0300

    deployment - vire changes

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,11 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            manifest: true,
         }),
     ],
     build: {
         manifest: true,
         outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            },
-        },
     },
     resolve: {
         alias: {
@@ -24,73 +18,3 @@ export default defineConfig({
         },
     },
 });
-
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-//     build: {
-//         manifest: true,
-//         outDir: 'public/build',
-//     }
-// });
-
-
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
-
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-//     server: {
-//         hmr: {
-//             host: 'localhost',
-//         },
-//     },
-// });
-
-
-
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-//     // Remove the server configuration for production
-//     // server: {
-//     //     hmr: {
-//     //         host: 'localhost',
-//     //     },
-//     // },
-// });
